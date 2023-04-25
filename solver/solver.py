8089d050fccc5fd73bd100a95c708aa43788e910
unknown <loumantau@gmail.com>
2023-04-24 22:14:07 -0300

    improved arguments

--- solver/solver.py
+++ solver/solver.py
@@ -1,27 +1,33 @@
+from scipy.constants import physical_constants
+
 class Solver():
  
     @staticmethod 
-    def voltage(current, resistance):
+    def voltage(**kwargs):
+        current = kwargs.get('current')
+        resistance = kwargs.get('resistance')
         return current * resistance
 
     @staticmethod
-    def current(voltage, resistance):
+    def current(**kwargs):
+        voltage = kwargs.get('voltage')
+        resistance = kwargs.get('resistance')
         return voltage / resistance
 
-    def resistance(voltage, current):
+    @staticmethod
+    def resistance(**kwargs):
+        voltage = kwargs.get('voltage')
+        current = kwargs.get('current')
         return voltage / current    
      
     @staticmethod
-    def ohms_law(solve_for, voltage=None, current=None, resistance=None):
+    def ohms_law(solve_for, **kwargs):
         method = {
             'voltage': Solver.voltage,
             'current': Solver.current,
             'resistance': Solver.resistance,
         } 
 
-        def solve(solve_for):
-            method[solve_for](solve_for, voltage=None, current=None, resistance=None)   
-
-        result = solve(solve_for)
+        result = method[solve_for](**kwargs)
 
         return result
